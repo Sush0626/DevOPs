d98facfb933ca4a5d74abe1d90979a64a4b126de
Sush0626 <158825064+Sush0626@users.noreply.github.com>
2024-05-24 15:17:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var counter = 0;
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", (Func<string>)(() => "Welcome to .NET 6 Minimal API with C# 10!"));
+app.MapGet("/counter", (Func<int>)(() => ++counter ));
+app.Run();
